98c516f0a2a50a40bf48e4a17315abb7f58c70c2
buckeyez <16466948+buckeyez@users.noreply.github.com>
2025-05-25 23:11:13 -0700

    init dev container

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+echo ".NET Version: $(dotnet --version)"
+echo "Node.js Version: $(node --version)"
+echo "npm Version: $(npm --version)"
+
+echo "Container setup complete!"
